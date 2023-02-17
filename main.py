aa108d4859481af04d8e36f04d957512a4894862
Grigoriy Mikheev <grigoriymihtt@gmail.com>
2023-02-18 02:52:44 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from telebot.async_telebot import AsyncTeleBot
+
+API_TOKEN = '5997480587:AAHDMpfgj-sUXkjfnES_umWbLgQU1AR4H_s'
+
+bot = AsyncTeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
