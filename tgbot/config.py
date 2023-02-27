bd80a9c7812c7438715ec238ec54a041a3fb7054
Grigoriy Mikheev <grigoriymihtt@gmail.com>
2023-02-27 14:24:13 +0300

    add db and collections from config

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,1 +1,4 @@
 TOKEN = '5997480587:AAHDMpfgj-sUXkjfnES_umWbLgQU1AR4H_s'
+mongo_database = 'tgbot'
+places_collection = 'places'
+users_collection = 'users'
--- tgbot/utils/database.py
+++ tgbot/utils/database.py
@@ -1,7 +1,7 @@
-from typing import List, Tuple, Optional, Dict
-import pprint
+from typing import List, Tuple
 from pymongo import MongoClient, GEO2D
 from tgbot.types.types import Place
+from tgbot.config import mongo_database, places_collection
 import logging
 
 
@@ -10,17 +10,17 @@ class Database:
         logging.basicConfig(filename='db.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',
                             level=logging.INFO)
         self.client = MongoClient('localhost', 27017)
-        self.db = self.client.tgbot  # init db
-        self.places = self.db.places  # init places collections
+        self.db = self.client[mongo_database]  # init db
+        self.places = self.db[places_collection]  # init places collections
         self.places.create_index([("coordinates", GEO2D)])
 
     def add_place(self, place: Place) -> str:
         logging.info("add: " + str(place))
-        return self.places.insert_one(place)
+        return self.places.insert_one(place).inserted_id
 
     def add_places(self, places: List[Place]) -> List[str]:
         logging.info("add: " + str(places))
-        return self.places.insert_many(places)
+        return self.places.insert_many(places).inserted_ids
 
     def get_all_places(self) -> List[Place]:
         return list(self.places.find())
